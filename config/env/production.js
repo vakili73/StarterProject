a0ed761fdeb47df19f4377735b4f699e4623e893
Vahid Vakili <v.vakili73@gmail.com>
2018-06-16 23:25:26 +0430

    Heroku Added...

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,10 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'http://localhost',
+      'https://evening-stream-26892.herokuapp.com/',
+    ],
 
 
     /***************************************************************************
